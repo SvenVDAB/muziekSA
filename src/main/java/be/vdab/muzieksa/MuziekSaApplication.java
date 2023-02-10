321b3284fb165deddff07b0bb2a16bbc56aec82f
SvenBouwen <sven.bouwen@vdabcampus.be>
2023-02-10 12:58:42 +0100

    Initial commit

---
+++ src/main/java/be/vdab/muzieksa/MuziekSaApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.muzieksa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuziekSaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuziekSaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/vdab/muzieksa/MuziekSaApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.muzieksa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuziekSaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
