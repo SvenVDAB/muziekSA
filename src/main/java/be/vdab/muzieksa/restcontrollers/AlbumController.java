4722f4d4586f7ed05e32abee2d721dfecaa0c134
SvenBouwen <sven.bouwen@vdabcampus.be>
2023-03-02 11:32:58 +0100

    hfdstk19

--- src/main/java/be/vdab/muzieksa/restcontrollers/AlbumController.java
+++ src/main/java/be/vdab/muzieksa/restcontrollers/AlbumController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/albums")
 @ExposesResourceFor(Album.class)
+@CrossOrigin
 public class AlbumController {
     private final AlbumService albumService;
     private final TypedEntityLinks.ExtendedTypedEntityLinks<Album> links;
